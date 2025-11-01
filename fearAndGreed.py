6757652ac51f727cdf1624d8dcdd73edb875b344
FastAndFunky <97684307+FastAndFunky@users.noreply.github.com>
2025-11-01 11:55:44 +0100

    Update fearAndGreed.py
    Incorrect variable name for secrets, fixed.

--- fearAndGreed.py
+++ fearAndGreed.py
@@ -16,8 +16,9 @@ HEADERS = {
 }
 
 # ---------------- Telegram Config ----------------
-TELEGRAM_BOT_TOKEN = "<TELEGRAM_TOKEN>"
-TELEGRAM_CHAT_ID = "<CHAT_ID>"
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram(msg: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
